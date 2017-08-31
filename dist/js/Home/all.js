06d5f230ec11f09a3294f00548b251896da85245
18320867985 <huangqishanyx@163.com>
2017-08-31 19:57:22 +0800

    2017-8-31 19:50

--- dist/js/Home/all.js
+++ dist/js/Home/all.js
@@ -9979,67 +9979,46 @@ window.$ === undefined && (window.$ = Zepto)
 
 /*
  *	公共类库
- */
-
-// 对Date的扩展，将 Date 转化为指定格式的String
-// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
-// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
-// 例子： 
-// (new Date()).Format("yyyy-MM-dd HH:mm:ss.S") ==> 2006-07-02 08:09:04.423 
-// (new Date()).Format("yyyy-M-d H:m:s.S")      ==> 2006-7-2 8:9:4.18 
-Date.prototype.format = function(fmt) { //author: meizz 
-	var o = {
-		"M+": this.getMonth() + 1, //月份 
-		"d+": this.getDate(), //日 
-		"H+": this.getHours(), //小时 
-		"m+": this.getMinutes(), //分 
-		"s+": this.getSeconds(), //秒 
-		"q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-		"S": this.getMilliseconds() //毫秒 
-	};
-	if(/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-	for(var k in o)
-		if(new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-	return fmt;
-}
-
-common = (function($) {
-
+*/
+	
+	
+ common=(function($){
+	
 	/***url对象***/
-	var url_fn = {
-		//采用正则表达式获取地址栏参数：（ 强烈推荐，既实用又方便！）
-		GetQueryString: function GetQueryString(name) {
-			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-			var r = window.location.search.substr(1).match(reg);
-			if(r != null) return unescape(r[2]);
-			return null;
-		},
+	var url_fn={
+			//采用正则表达式获取地址栏参数：（ 强烈推荐，既实用又方便！）
+			GetQueryString:function GetQueryString(name) {
+			    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+			    var r = window.location.search.substr(1).match(reg);
+			    if (r != null) return unescape(r[2]); return null;
+			},
+			
+			//从WebAPI获取日期json数据 转换成日期时间戳
+			jsonToDate:function jsonToDate(apidate) {
+	  		  var txts = apidate.replace("/Date(", "").replace(")/");
+	   		 return parseInt(txts.trim());
+	
+			},
+			
+			
+			// 取当前页面名称(不带后缀名)
+   			getPageName:function getPageName() {
+	  		  var a = location.href;
+	  		  var b = a.split("/");
+	 		  var c = b.slice(b.length - 1, b.length).toString(String).split(".");
+	  		  return c.slice(0, 1);
+			},
 
-		//从WebAPI获取日期json数据 转换成日期时间戳
-		jsonToDate: function jsonToDate(apidate) {
-			var txts = apidate.replace("/Date(", "").replace(")/");
-			return parseInt(txts.trim());
-
-		},
-
-		// 取当前页面名称(不带后缀名)
-		getPageName: function getPageName() {
-			var a = location.href;
-			var b = a.split("/");
-			var c = b.slice(b.length - 1, b.length).toString(String).split(".");
-			return c.slice(0, 1);
-		},
-
-		//取当前页面名称(带后缀名)
-		getPageNameExention: function getPageNameExention() {
-			var strUrl = location.href;
-			var arrUrl = strUrl.split("/");
-			var strPage = arrUrl[arrUrl.length - 1];
-			return strPage;
-		},
-
+			//取当前页面名称(带后缀名)
+			getPageNameExention:function getPageNameExention() {
+	  		  var strUrl = location.href;
+	  		  var arrUrl = strUrl.split("/");
+	  		  var strPage = arrUrl[arrUrl.length - 1];
+	  		  return strPage;
+			},
+			
 	}
-
+	
 	/**
 	 * 延迟加载
 	 *  * <img class="load-lazy"
@@ -10049,68 +10028,81 @@ common = (function($) {
 	 * > 
 	 * */
 	// 延迟加载
-	var jqlazy_fn = function() {
-
+	var jqlazy_fn=function(){
+		
 		var window_h = $(window).height();
+				
+				$(window).scroll(function() {
+				
+					setTimeout(function() {
+						
+						$(".load-lazy").each(function() {
+						
+							var img_h = parseInt($(this).offset().top) - parseInt(window_h);
+							var img_h2 = parseInt($(this).offset().top)+$(this).height();
+							if($(document).scrollTop() >= img_h&&$(document).scrollTop()<img_h2) {
 
-		$(window).scroll(function() {
-
-			setTimeout(function() {
-
-				$(".load-lazy").each(function() {
-
-					var img_h = parseInt($(this).offset().top) - parseInt(window_h);
-					var img_h2 = parseInt($(this).offset().top) + $(this).height();
-					if($(document).scrollTop() >= img_h && $(document).scrollTop() < img_h2) {
-
-						$(this).attr("src", $(this).attr("data-src"));
-
-						/*ie8 不支持
-						 * .animate({
-						"opacity":0.2
-						}).animate({
-						"opacity": 1
-						}, 500);
+								$(this).attr("src", $(this).attr("data-src"));
+							
+								/*ie8 不支持
+								 * .animate({
+								"opacity":0.2
+								}).animate({
+								"opacity": 1
+								}, 500);
 								
-						* */
+								* */
 
-					}
+							}
 
+						})
+					}, 100)
 				})
-			}, 100)
-		})
-	};
-
+			};
+	
+	
+	
+	
+	
 	/*返回对象*/
-	return {
-
-		url: url_fn,
-		lazy: {
-			jqlazy: jqlazy_fn
+	 return{
+	 	
+	 	url:url_fn,
+		lazy:{
+			jqlazy:jqlazy_fn
 		},
+		
+		}
+	 
+	
+})(window.jQuery||window.Zepto);
 
-	}
 
-})(window.jQuery || window.Zepto);
+
 
 /*
  * requireJs amd 模块加载
  */
 
-if(typeof define === "function" && define.amd) {
-	define("common", [], function() {
+if ( typeof define === "function" && define.amd ) {
+	define( "common", [], function() {
 		return common;
 	});
 }
+
 
 /*
  * es6 模块加载
  */
 //
-if(typeof exports === 'object' && typeof module !== 'undefined') {
-
-	module.exports.common = common;
+if(typeof exports === 'object' && typeof module !== 'undefined'){
+	
+	 module.exports.common=common;
 }
+
+
+
+
 
 
 	mui.init({
