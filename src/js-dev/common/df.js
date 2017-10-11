407cf284343d3b1d9b877589a97171a5f6c0398f
18320867985 <huangqishanyx@163.com>
2017-10-11 18:01:36 +0800

    2017-10-11

--- src/js-dev/common/df.js
+++ src/js-dev/common/df.js
@@ -1,32 +1,31 @@
-
-
-	mui.init({
-				swipeBack: true //启用右滑关闭功能
-	});
+mui.init({
+	swipeBack: true //启用右滑关闭功能
+});
 //跳转的链接
 $(".mui-bar.mui-bar-tab .mui-tab-item").on("tap", function() {
 	//	mui('.mui-popover').popover('hide'); //show hide toggle
-	window.location.href = this.href ;
+	window.location.href = this.href;
 
 });
-
 
 //发布报价
 $(".fabulist-nav .fabu-nav .mui-scroll .mui-control-item").on("tap", function() {
-	
-	window.location.href = this.href ;
+
+	window.location.href = this.href;
 
 });
 
-
-
 //兼容mobile
-$("head meta").each(function(){
-	
-	var metaVal=$(this).attr("name");
-	if(metaVal==="viewport"){
-		$(this).attr("content","width=device-width, initial-scale=1, maximum-scale=1.0 ");
+$("head meta").each(function() {
+
+	var metaVal = $(this).attr("name");
+	if(metaVal === "viewport") {
+		$(this).attr("content", "width=device-width, initial-scale=1, maximum-scale=1.0 ");
 	}
 });
 
+// 拨打电话
+$(".callTell").on("tap", function() {
 
+	window.location.href = 'tel:' + $(this).find(".tel").text();
+});
---
+++ src/js-dev/modules/baojia-gd.js
@@ -0,0 +1,36 @@
+
+var baojia_gd=(function($){
+	
+	var init=function(){
+		
+			//选择的样式1
+				$(".baojia-gd-cont-box1 li").on("tap", function() {
+
+					$(".baojia-gd-cont-box1 li").removeClass("active");
+					$(".baojia-gd-cont-box2 li").removeClass("active");
+					$(this).addClass("active");
+				});
+
+				//选择的样式2
+				$(".baojia-gd-cont-box2 li").on("tap", function() {
+
+					$(".baojia-gd-cont-box2 li").removeClass("active");
+					$(".baojia-gd-cont-box1 li").removeClass("active");
+					$(this).addClass("active");
+				});
+
+				$(".baojia-gd-cont-box1 .btn1").on("click", function() {
+					$(this).toggleClass("active");
+					$(".baojia-gd-cont-box2").toggle();
+				});
+
+				
+		
+	}
+	
+	return {
+		init:init
+	}
+	
+	
+})(window.Zepto);
