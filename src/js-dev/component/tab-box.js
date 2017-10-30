d5721ea915e35f8985788b5eb727a63d1665736d
18320867985 <huangqishanyx@163.com>
2017-10-30 15:57:38 +0800

    2017-10-30

--- src/js-dev/component/tab-box.js
+++ src/js-dev/component/tab-box.js
@@ -12,7 +12,7 @@
 
 ;(function($) {
 
-	// 选项卡tag-box
+	// 选项卡tag-box 1
 	$(".tab-box .tab-item").on("click", function(e) {
 		
 		e.preventDefault();
@@ -22,6 +22,23 @@
 		var target = $(this).attr("data-target");
 		$(".tab-content").find(".tab-content-item").removeClass("active");
 		$(".tab-content").find(target).addClass("active");
+		
+		// 点击触发自定义事件 
+		$(this).trigger("tab_select");
+
+	});
+	
+	// 选项卡tag-box2 新的
+	$(".tab-big .tab-ttl .tab-item").on("click", function(e) {
+		
+		e.preventDefault();
+		var p=$(this).parents(".tab-big");
+		p.find(".tab-ttl .tab-item").removeClass("active");
+		$(this).addClass("active");
+
+		var target = $(this).attr("data-target");
+		$(".tab-content",p).find(".tab-content-item").removeClass("active");
+		$(".tab-content",p).find(target).addClass("active");
 		
 		// 点击触发自定义事件 
 		$(this).trigger("tab_select");
